3418fc360fac1eba220ba90d05d010214db20740
Richard Marr <richard.marr@gmail.com>
2012-10-15 07:51:18 +0100

    Testing to see why the CSS isn't coming through

--- go.js
+++ go.js
@@ -6,7 +6,7 @@ if (typeof jQuery == 'undefined'){
 setTimeout(function(){
 	jQuery(function($){
 		// Set up output
-		$("<link href='//raw.github.com/richmarr/go-crawly/master/go.css' media='screen' rel='Stylesheet' type='text/css'>").appendTo(document.head);
+		$("<link href='//raw.github.com/richmarr/go-crawly/master/go.css' media='screen' rel='Stylesheet' type='text/css'>").appendTo(document.body);
 		var $gc = $("<div id='gocrawly'></div>").appendTo(document.body);
 		
 		// Seed the queue
