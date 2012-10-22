9b7f8787a274aaf1434eba60cde69aae1da16e59
Richard Marr <richard.marr@gmail.com>
2012-10-22 21:44:30 +0100

    Moved to using events. Currently synchronous, no concurrent requests

--- go.js
+++ go.js
@@ -8,14 +8,14 @@ setTimeout(function(){
 		
 		// Output style
 		$("<style>\n"+
-			"#gocrawly { padding:10px; margin:10px; background:rgba(255,255,255,0.3); border:3px solid white; border-radius:10px; box-shadow:2px 2px 15px #666, 2px 2px 15px #eee inset; color:#999; position:absolute; right:0; top:0; width:400px; z-index:9999999 }\n"+
+			"#gocrawly { padding:10px; margin:10px; background:rgba(255,255,255,0.8); border:3px solid white; border-radius:10px; box-shadow:2px 2px 15px #666, 2px 2px 15px #eee inset; color:#999; position:absolute; right:0; top:0; width:400px; z-index:9999999 }\n"+
 			"#gocrawly .page { font:11px Helvetica, Arial; padding:1px; margin:1px; }\n"+
 			"	#gocrawly .page a { color:#39c}\n"+
 			"	#gocrawly .page ul { margin:0 0 0 10px; padding:0 }\n"+
 			"		#gocrawly .page li { margin:0; padding:0; list-style:none }\n"+
 			"			#gocrawly .page li a { color:#9cf; font-size:10px }\n"+
 			"#gocrawly .queued { background:#999; color:#666 }\n"+
-			"#gocrawly .running { background:#ccc; color:#696 }\n"+
+			"#gocrawly .calling { background:#ccc; color:#696 }\n"+
 			"#gocrawly .done { color:#666 }\n"+
 			"#gocrawly .error { background:#fcc }\n"+
 			"#gocrawly .resources { display:none; overflow:hidden }\n"+
@@ -29,18 +29,17 @@ setTimeout(function(){
 			"</style>").appendTo(document.head);
 		
 		// outline UI
-		var $gc = $("<div id='gocrawly'></div>").appendTo(document.body);
+		var $gc = $("<div id='gocrawly'></div>")
+			.appendTo(document.body)
+			.bind('next',function(){$(".queued:first",this).trigger('call')});
 		$("<a href='#' style='float:right'>X</a>").appendTo($gc).click(function(e){ e.preventDefault(); $gc.remove() });
 		
-		// Seed the queue
-		var queue = [],
-			startTag = /<(body|head)[^>]*>/ig,
+		
+		var startTag = /<(body|head)[^>]*>/ig,
 			endTag = /<\/(body|head)[^>]*>/ig,
 			queued = {},
 			resources = {},
-			throttle = 2,
-			open = 0,
-			maxDepth = 6,
+			maxDepth = 5,
 			blockedExtentions = /\.(zip|png|gif|je?pg|pdf|docx?|xlsx?|ppsx?|mov|avi|js|javascript|xml|sh|txt)$/,
 			running = false,
 			origin = parseUrl(document.location.href),
@@ -56,56 +55,10 @@ setTimeout(function(){
  			return a;
 		}
 		
+		
 		enqueue(origin);
-		run()
+		$gc.trigger('next');
 		
-		// Iterates over the queue
-		function run(){
-			if ( !running ) {
-				running = true; 
-				while( queue.length && open < throttle ) {
-					get( queue.shift() ); // FIFO
-					open++;
-				}
-				running = false;
-			}
-		}
-		
-		// Takes some response data and pulls any links or resources out of it
-		function handle( link, data ){
-			// HTML comes in as a string, XHTML comes in as a document, and we want to avoid auto pre-loading all the images
-			var $data = data ? $( data instanceof Document ? data : data.replace(/[ ]src=/ig," data-src=").replace(startTag,"<div>").replace(endTag,"</div>") ) : undefined,
-				count = 0;
-			
-			// Queue up any links in this doc
-			$( "a", $data ).each(function(){ enqueue( parseUrl($(this).attr("href"), link.depth+1 ) ); });
-			
-			// List all the resources
-			$("img,link,script",$data).each(function(){ if ( resource( link, this ) ) count++; });
-			
-			$("<a href='#'>("+count+")</a>").appendTo("#"+link.id+" .total").click(function(e){
-				e.preventDefault();
-				$("#"+link.id+" .resources").stop().slideToggle();
-			});
-			
-			// Kick the loop off
-			setTimeout( run, 1 );
-		}
-		
-		// calls URLs to and wraps the source link into the processing call
-		function get( link ){
-			var $link = $("#"+link.id).removeClass("queued").addClass("running");
-			var start = new Date();
-			var $req = $.get( link.path, function(data){ 
-				open--;
-				var time = new Date().getTime() - start.getTime();
-				$link.removeClass("running").addClass("done").find(".time").html(time+"ms");
-				handle( link, data ); 
-			},'text').error( function( xhr, status, err ){ 
-				open--;
-				$link.removeClass("running").addClass("error").append("<div>"+err+"</div>");
-			});
-		}
 		
 		// push a new link to the queue
 		function enqueue( a ){
@@ -118,33 +71,89 @@ setTimeout(function(){
 			
 			// output
 			$("<div class='page depth_"+a.depth+" queued' data-depth='"+a.depth+"'>"+
-				"<a href='"+a.path+"'>"+a.path+"</a> "+
-				"<span class='time'></span> "+
-				"<span class='total'></span> "+
-				"<ul class='resources'></ul> "+
-			"</div>")
-				.appendTo($gc);
+					"<a class='url' href='"+a.path+"'>"+a.path+"</a> "+
+					"<span class='time'></span> "+
+					"<span class='total'></span> "+
+					"<ul class='resources'></ul> "+
+				"</div>")
+				.appendTo($gc)
+				.bind('call',get)
+				.bind('done',handleResponse);
 			
-			queue.push(a);
 			queued[a.path] = true;
 		}
+			
+		// Call down fresh HTML
+		function get(e){
+			
+			var start = new Date().getTime(),
+				$this = $(this).removeClass("queued").addClass('calling');
+			
+			$.ajax( $this.find("a.url").text(), {
+				error:function( xhr, status, err ){
+					// Just notification for now but this could handle redirects later
+					console && console.error(err);
+					$this.addClass("error").append("<div>"+err+"</div>");
+					$gc.trigger('next')
+				},
+				success: function( data, status, xhr ){
+					var time = new Date().getTime() - start;
+					$this.addClass("done")
+						.trigger("done",data)
+						.find(".time").html(time+"ms");
+				},
+				complete:function(xhr, status){
+					$this.removeClass("calling");
+				},
+				converters:{
+					"* text": window.String,
+					"text html": true,
+					"text xml": true
+				},
+				dataType:'html'
+			});
+		}
 		
-		function resource( link, node ){
-			var name = node.tagName.toLowerCase(),
-				url;
-			switch ( name ) {
-				case 'script':
-				case 'img':
-					url = node.getAttribute('data-src');
-					break;
-				case 'link':
-					url = node.getAttribute('href');
-					break;
-			}
-			if ( url ) {
-				$("#"+link.id+" .resources").append("<li><a href='"+url+"'>"+url+"</a></li>");
-				return true
-			}
+		
+		
+		// Takes some response data and pulls any links or resources out of it
+		function handleResponse( e, data ){
+			
+			// HTML comes in as a string, XHTML comes in as a document, and we want to avoid auto pre-loading all the images
+			var $data = data ? $( data instanceof Document ? data : data.replace(/[ ]src=/ig," data-src=").replace(startTag,"<div>").replace(endTag,"</div>") ) : undefined,
+				count = 0,
+				$this = $(this);
+				
+			// Queue up any links in this doc
+			$( "a", $data ).each(function(){ enqueue( parseUrl($(this).attr("href"), parseInt($this.attr('data-depth'))+1 ) ); });
+			
+			// List all the resources
+			$("img,link,script",$data).each(function(){
+				var name = this.tagName.toLowerCase(),
+					url;
+				switch ( name ) {
+					case 'script':
+					case 'img':
+						url = this.getAttribute('data-src');
+						break;
+					case 'link':
+						url = this.getAttribute('href');
+						break;
+				}
+				if ( url ) {
+					$(".resources",$this).append("<li><a href='"+url+"'>"+url+"</a></li>");
+					count++;
+				}
+			});
+			
+			$("<a href='#'>("+count+")</a>").appendTo($this.find(".total")).click(function(e){
+				e.preventDefault();
+				$(".resources",$this).stop().slideToggle();
+			});
+			
+			$gc.trigger('next');
+			// Kick the loop off
+			//setTimeout( run, 1 );
 		}
 		
 		// Random base 52 ID
